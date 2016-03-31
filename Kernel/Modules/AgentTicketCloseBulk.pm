5f9c12d0090400d890345d312f406c6e29a8b295
Patrick Rauscher <prauscher@prauscher.de>
2016-03-31 15:32:50 +0200

    Fix error when using QuickClose with pending state (Can't call method "SystemTime" on an undefined value)

--- Kernel/Modules/AgentTicketCloseBulk.pm
+++ Kernel/Modules/AgentTicketCloseBulk.pm
@@ -249,7 +249,7 @@ sub Run {
                     ( 1 * 24 * 60 );
     
                 my ($Sec, $Min, $Hour, $Day, $Month, $Year) = $TimeObject->SystemTime2Date(
-                    SystemTime => $Self->{TimeObject}->SystemTime() + ( $Diff * 60 ),
+                    SystemTime => $TimeObject->SystemTime() + ( $Diff * 60 ),
                 );
 
                 if ( $CloseData{FixHour} ) {
